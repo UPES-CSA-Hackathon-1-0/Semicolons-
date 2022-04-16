55e08a52e927c1748924a1bf46868b0429a69197
jatin66 <75089053+jatin66@users.noreply.github.com>
2022-04-16 13:25:09 +0530

    Add files via upload

---
+++ index1copy.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+</body>
+</html>
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>REGISTER</title>
+      
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="CSS/navbar.css">
+      <link rel="stylesheet" href="CSS/register.css">
+
+   </head>
+   <body>
+    <!-- <nav>
+        <div class="logo">
+           MODULE
+        </div>
+        <input type="checkbox" id="click">
+        <label for="click" class="menu-btn">
+        <i class="fas fa-bars"></i>
+        </label>
+        <ul>
+           
+           <li><a class="active" href="#">Register</a></li>
+           <li><a href="login.html">Login</a></li>
+          
+        </ul>
+     </nav> -->
+      <div class="content">
+        <h1 class="topheading"></h1>
+   
+        <div class="box" >
+            <h2 class="secondheading">Register Now</h2>
+                
+            <form action="index2.php" method="post" class="input1">
+                <div style="align-items: center;">
+                    <input type="text" name="name" placeholder="Enter your Article" >
+                    <!-- <input type="Phone" name="Phone" placeholder="Enter your Phone number" class="input"> -->
+                    <!-- <input type="Email" name="email" placeholder="Enter your email" class="input">
+                    <input type="text" name="sap" placeholder="Enter your SAPID" class="input">
+                    <input type="text" name="desig" placeholder="Enter your DESIGNATION" class="input">
+                    <input type="text" name="username" placeholder="Enter your username" class="input">
+                    <input type="Password" name="password" placeholder="Set your Pasword" class="input">
+                    <input type="Password" name="cpassword" placeholder="Confirm your Pasword" class="input"> -->
+                    
+                    <input type="submit" class="submit" >
+    
+                </div>
+    
+            </form>
+        </div>
+        
+        <br>
+        <br>
+        <footer style="background-color: black; color:white;text-align:center;height:80px;">
+            <span ><br><div class="logo"><h4>
+               COPYRIGHT @ANKUR SINGH</h4>
+             </div>
+            </span>
+            </footer>
+      </div>
+
+   </body>
+</html>
---
+++ login.php
@@ -0,0 +1,47 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/navbar.css">
+    <link rel="stylesheet" href="CSS/login.css">
+</head>
+<body>
+    
+      <div class="content">
+        <h1 class="topheading"></h1>
+   
+        <div class="box" >
+            <h2 class="secondheading">LOGIN</h2>
+                    
+            <form action="index1.php" method="post" class="input1">
+                <div style="align-items: center;">
+                    <input type="text" name="username" placeholder="Enter your Username" class="input topinput">
+                  
+                    <input type="Password" name="password" placeholder="Enter your Pasword" class="input">
+                    
+                    <input type="submit" class="submit" >
+    
+                </div>
+    
+            </form>
+        </div>
+        
+        <br>
+        <br>
+        <footer style="background-color: black; color:white;text-align:center;height:80px;">
+            <span ><br><div class="logo"><h4>
+               COPYRIGHT @ANKUR SINGH</h4>
+             </div>
+            </span>
+            </footer>
+      </div>
+    
+</body>
+</html>  
